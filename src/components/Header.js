54c79e4fbae87af562fceeb8aadd9159d5f7a60c
saurabh <emosrb@gmail.com>
2022-07-09 11:37:45 -0500

    Created a component and passed a prop and a variable

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export const Header = (props) => {
+    const exampleTest = 'Example';
+    return(
+        <header>
+            <h1>{props.title} with an {exampleTest}</h1>
+        </header>
+        )
+}
